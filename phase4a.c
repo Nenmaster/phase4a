2140eafcb11a386f8103238ea484d9d5815e1d23
Omar <omendivil8@gmail.com>
2025-04-24 12:32:55 -0700

    ayman version with names updated

--- phase4a.c
+++ phase4a.c
@@ -13,7 +13,6 @@
 #define MAX_LINES 10
 #define MAX_LINE_LENGTH 80
 
-// ----- Globals and Structures -----
 typedef struct SleepRequest {
   unsigned int wakeupTime;
   int pid;
@@ -22,25 +21,24 @@ typedef struct SleepRequest {
 
 SleepRequest *sleepQueue = NULL;
 int clockMailbox;
-int clockTicks = 0;
 
 int termWriteLock[4];
 int termReadMailbox[4];
 int termInterruptMailbox[4];
-int termWriterPid[USLOSS_TERM_UNITS];
+int termWritePid[USLOSS_TERM_UNITS];
+
 int readDoneMailbox;
-int writeDoneMailbox;
-char writeBuffer[4][MAX_LINE_LENGTH];
-int writeLength[4];
-int writeIndex[4];
-int writeReady[4];
 
-char lineBuffer[4][MAX_LINE_LENGTH];
-int lineLength[4];
+char writeBuff[4][MAX_LINE_LENGTH];
+int writeLen[4];
+int writIdx[4];
+int writeWaiting[4];
 
-// prototypes
-void addSleepRequest(int pid, int wakeupTime);
-void checkWakeups();
+char lineBuff[4][MAX_LINE_LENGTH];
+int lineLen[4];
+
+void enqueueSleepRequest(int pid, int wakeupTime);
+void wakeUpProc(unsigned int currTime);
 int ClockDriver(void *arg);
 int TerminalDriver(void *arg);
 void sleepHandler(USLOSS_Sysargs *args);
@@ -53,12 +51,8 @@ void phase4_start_service_processes(void) {
   for (int i = 0; i < 4; i++) {
     spork("TermDriver", TerminalDriver, (void *)(long)i, USLOSS_MIN_STACK, 1);
   }
-
   for (int i = 0; i < 4; i++) {
-    int ctrl = 0;
-    ctrl |= USLOSS_TERM_CTRL_RECV_INT(0);
-    ctrl |= USLOSS_TERM_CTRL_XMIT_INT(0);
-    // printf("phase4_start_service: unit %d ctrl=0x%04x\n", i, ctrl);
+    int ctrl = USLOSS_TERM_CTRL_RECV_INT(0) | USLOSS_TERM_CTRL_XMIT_INT(0);
     USLOSS_DeviceOutput(USLOSS_TERM_DEV, i, (void *)(long)ctrl);
   }
 }
@@ -67,22 +61,20 @@ void phase4_init(void) {
   systemCallVec[SYS_SLEEP] = sleepHandler;
   systemCallVec[SYS_TERMWRITE] = termWriteHandler;
   systemCallVec[SYS_TERMREAD] = termReadHandler;
-
   USLOSS_IntVec[USLOSS_TERM_INT] = termHandler;
 
   clockMailbox = MboxCreate(1, 0);
   readDoneMailbox = MboxCreate(1, 0);
-  writeDoneMailbox = MboxCreate(1, 0);
   for (int i = 0; i < 4; i++) {
     int semId;
     termWriteLock[i] = kernSemCreate(1, &semId);
     termReadMailbox[i] = MboxCreate(MAX_LINES, MAX_LINE_LENGTH);
-    termInterruptMailbox[i] = MboxCreate(8, sizeof(int)); // 👈 Add this line
-    termWriterPid[i] = -1;
-    writeLength[i] = 0;
-    writeIndex[i] = 0;
-    writeReady[i] = 0;
-    lineLength[i] = 0;
+    termInterruptMailbox[i] = MboxCreate(8, sizeof(int));
+    termWritePid[i] = -1;
+    writeLen[i] = 0;
+    writIdx[i] = 0;
+    writeWaiting[i] = 0;
+    lineLen[i] = 0;
   }
 }
 
@@ -95,7 +87,7 @@ int ClockDriver(void *arg) {
 
     USLOSS_DeviceInput(USLOSS_CLOCK_DEV, 0, (int *)&currTime);
 
-    checkWakeups(currTime);
+    wakeUpProc(currTime);
   }
   return 0;
 }
@@ -103,90 +95,58 @@ int ClockDriver(void *arg) {
 void termHandler(int type, void *arg) {
   int unit = (int)(long)arg;
   int status;
-
   USLOSS_DeviceInput(USLOSS_TERM_DEV, unit, &status);
-
-  int result = MboxCondSend(termInterruptMailbox[unit], &status, sizeof(int));
+  MboxCondSend(termInterruptMailbox[unit], &status, sizeof(int));
 }
 
 int TerminalDriver(void *arg) {
   int unit = (int)(long)arg;
   int status;
-
   while (1) {
-    int result = MboxRecv(termInterruptMailbox[unit], &status, sizeof(int));
-    if (result != sizeof(int)) {
+    if (MboxRecv(termInterruptMailbox[unit], &status, sizeof(int)) !=
+        sizeof(int))
       continue;
-    }
 
-    int xmit_status = USLOSS_TERM_STAT_XMIT(status);
-
-    if (xmit_status == USLOSS_DEV_READY) {
-
+    if (USLOSS_TERM_STAT_XMIT(status) == USLOSS_DEV_READY) {
       kernSemP(termWriteLock[unit]);
-      int wasReady = writeReady[unit];
-
-      if (wasReady) {
-        if (writeIndex[unit] < writeLength[unit]) {
-          char ch = writeBuffer[unit][writeIndex[unit]++];
+      if (writeWaiting[unit]) {
+        if (writIdx[unit] < writeLen[unit]) {
+          char ch = writeBuff[unit][writIdx[unit]++];
           kernSemV(termWriteLock[unit]);
-
-          int ctrl = 0;
-          ctrl = USLOSS_TERM_CTRL_CHAR(ctrl, ch);
+          int ctrl = USLOSS_TERM_CTRL_CHAR(0, ch);
           ctrl = USLOSS_TERM_CTRL_XMIT_INT(ctrl);
           ctrl = USLOSS_TERM_CTRL_RECV_INT(ctrl);
           ctrl |= USLOSS_TERM_CTRL_XMIT_CHAR(0);
-
-          int outPutRes =
-              USLOSS_DeviceOutput(USLOSS_TERM_DEV, unit, (void *)(long)ctrl);
-
+          USLOSS_DeviceOutput(USLOSS_TERM_DEV, unit, (void *)(long)ctrl);
         } else {
-          int pidToUnblock = termWriterPid[unit];
-          writeReady[unit] = 0;
-          termWriterPid[unit] = -1;
+          int pidToUnblock = termWritePid[unit];
+          writeWaiting[unit] = 0;
+          termWritePid[unit] = -1;
           kernSemV(termWriteLock[unit]);
-
-          if (pidToUnblock != -1) {
+          if (pidToUnblock != -1)
             unblockProc(pidToUnblock);
-          }
-          int final_ctrl = 0;
-          final_ctrl = USLOSS_TERM_CTRL_RECV_INT(final_ctrl);
-          final_ctrl = USLOSS_TERM_CTRL_XMIT_INT(final_ctrl);
-          USLOSS_DeviceOutput(USLOSS_TERM_DEV, unit, (void *)(long)final_ctrl);
+          int ctrl =
+              USLOSS_TERM_CTRL_RECV_INT(0) | USLOSS_TERM_CTRL_XMIT_INT(0);
+          USLOSS_DeviceOutput(USLOSS_TERM_DEV, unit, (void *)(long)ctrl);
         }
-      } else {
-        kernSemV(termWriteLock[unit]); // Release lock
-      }
+      } else
+        kernSemV(termWriteLock[unit]);
     }
 
-    int recv_status = USLOSS_TERM_STAT_RECV(status);
-    if (recv_status == USLOSS_DEV_BUSY) {
+    if (USLOSS_TERM_STAT_RECV(status) == USLOSS_DEV_BUSY) {
       char ch = USLOSS_TERM_STAT_CHAR(status);
-
-      if (lineLength[unit] < MAX_LINE_LENGTH) {
-        lineBuffer[unit][lineLength[unit]++] = ch;
+      if (lineLen[unit] < MAX_LINE_LENGTH)
+        lineBuff[unit][lineLen[unit]++] = ch;
+      if (ch == '\n' || lineLen[unit] == MAX_LINE_LENGTH) {
+        lineBuff[unit][lineLen[unit]] = '\0';
+        MboxSend(termReadMailbox[unit], lineBuff[unit], lineLen[unit]);
+        lineLen[unit] = 0;
       }
-
-      if (ch == '\n' || lineLength[unit] == MAX_LINE_LENGTH) {
-        lineBuffer[unit][lineLength[unit]] = '\0';
-        int send_res =
-            MboxSend(termReadMailbox[unit], lineBuffer[unit], lineLength[unit]);
-        if (send_res < 0) {
-          printf("TerminalDriver[unit %d]: MboxSend to termReadMailbox failed! "
-                 "Result = %d\n",
-                 unit, send_res);
-        }
-        lineLength[unit] = 0;
-      }
+      int ctrl = USLOSS_TERM_CTRL_RECV_INT(0);
       kernSemP(termWriteLock[unit]);
-      int needsXmitInt = writeReady[unit];
+      if (writeWaiting[unit])
+        ctrl |= USLOSS_TERM_CTRL_XMIT_INT(0);
       kernSemV(termWriteLock[unit]);
-
-      int ctrl = 0;
-      ctrl = USLOSS_TERM_CTRL_RECV_INT(ctrl);
-      if (needsXmitInt) {
-        ctrl = USLOSS_TERM_CTRL_XMIT_INT(ctrl);
-      }
       USLOSS_DeviceOutput(USLOSS_TERM_DEV, unit, (void *)(long)ctrl);
     }
   }
@@ -197,47 +157,36 @@ void termWriteHandler(USLOSS_Sysargs *args) {
   char *buf = (char *)args->arg1;
   int len = (int)(long)args->arg2;
   int unit = (int)(long)args->arg3;
-  int callingPid = getpid(); // Get PID of the process calling TermWrite
+  int pid = getpid();
 
-  if (!buf || len < 0 || len > MAX_LINE_LENGTH || unit < 0 ||
-      unit >= USLOSS_TERM_UNITS) {
+  if (!buf || len < 0 || len > MAX_LINE_LENGTH || unit < 0 || unit >= 4) {
     args->arg4 = (void *)(long)-1;
     return;
   }
 
   kernSemP(termWriteLock[unit]);
-
-  if (writeReady[unit]) {
-    kernSemV(termWriteLock[unit]);
-    printf("termWriteHandler[unit %d, pid %d]: Driver busy!\n", unit,
-           callingPid);
-    args->arg4 = (void *)(long)-1;
-    return;
-  }
-  memcpy(writeBuffer[unit], buf, len);
-  writeLength[unit] = len;
-  writeIndex[unit] = 0;
-  writeReady[unit] = 1;
-  termWriterPid[unit] = callingPid;
-
+  memcpy(writeBuff[unit], buf, len);
+  writeLen[unit] = len;
+  writIdx[unit] = 0;
+  writeWaiting[unit] = 1;
+  termWritePid[unit] = pid;
   kernSemV(termWriteLock[unit]);
 
-  if (writeLength[unit] > 0) {
-    char ch = writeBuffer[unit][writeIndex[unit]++];
-    int ctrl = 0;
-    ctrl = USLOSS_TERM_CTRL_CHAR(ctrl, ch);
+  if (len > 0) {
+    char ch = writeBuff[unit][writIdx[unit]++];
+    int ctrl = USLOSS_TERM_CTRL_CHAR(0, ch);
     ctrl = USLOSS_TERM_CTRL_XMIT_INT(ctrl);
     ctrl = USLOSS_TERM_CTRL_RECV_INT(ctrl);
     ctrl |= USLOSS_TERM_CTRL_XMIT_CHAR(0);
     USLOSS_DeviceOutput(USLOSS_TERM_DEV, unit, (void *)(long)ctrl);
-
     blockMe();
   } else {
     kernSemP(termWriteLock[unit]);
-    writeReady[unit] = 0;
-    termWriterPid[unit] = -1;
+    writeWaiting[unit] = 0;
+    termWritePid[unit] = -1;
     kernSemV(termWriteLock[unit]);
   }
+
   args->arg2 = (void *)(long)len;
   args->arg4 = (void *)(long)0;
 }
@@ -246,22 +195,17 @@ void termReadHandler(USLOSS_Sysargs *args) {
   char *buffer = (char *)args->arg1;
   int bufSize = (int)(long)args->arg2;
   int unit = (int)(long)args->arg3;
-  //  printf("in read handler\n");
   if (!buffer || bufSize <= 0 || unit < 0 || unit > 3) {
     args->arg4 = (void *)(long)-1;
     return;
   }
-
   char tempBuf[MAX_LINE_LENGTH];
-
   int len = MboxRecv(termReadMailbox[unit], tempBuf, MAX_LINE_LENGTH);
   if (len > bufSize)
     len = bufSize;
   memcpy(buffer, tempBuf, len);
-
   args->arg2 = (void *)(long)len;
   args->arg4 = (void *)(long)0;
-  MboxSend(readDoneMailbox, NULL, 0);
 }
 
 void sleepHandler(USLOSS_Sysargs *args) {
@@ -283,13 +227,12 @@ void sleepHandler(USLOSS_Sysargs *args) {
 
   wakeUpTime = currTime + ((unsigned long long)seconds * 1000000);
 
-  addSleepRequest(pid, wakeUpTime);
+  enqueueSleepRequest(pid, wakeUpTime);
   blockMe();
   args->arg4 = (void *)(long)0;
 }
 
-// ----- Helpers -----
-void addSleepRequest(int pid, int wakeupTime) {
+void enqueueSleepRequest(int pid, int wakeupTime) {
   SleepRequest *newReq = malloc(sizeof(SleepRequest));
   newReq->pid = pid;
   newReq->wakeupTime = wakeupTime;
@@ -309,7 +252,7 @@ void addSleepRequest(int pid, int wakeupTime) {
   curr->next = newReq;
 }
 
-void checkWakeups(unsigned int currTime) {
+void wakeUpProc(unsigned int currTime) {
   while (sleepQueue != NULL && sleepQueue->wakeupTime <= currTime) {
     SleepRequest *tmp = sleepQueue;
     sleepQueue = sleepQueue->next;
